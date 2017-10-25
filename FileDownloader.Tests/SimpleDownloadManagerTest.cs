2e40366b4f3edba52e2622394e98c7fa25ec8983
Sofia Huts <sofia.huts@gmail.com>
2017-10-25 23:43:29 +0300

    Add test on SimpleDownloadManager

---
+++ FileDownloader.Tests/SimpleDownloadManagerTest.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using FileDownloader.Downloaders;
+using FileDownloader.FileSystems;
+using FileDownloader.Managers;
+using Moq;
+using NUnit.Framework;
+
+namespace FileDownloader.Tests
+{
+    [TestFixture]
+    public class SimpleDownloadManagerTest
+    {
+        private Mock<IFileSystem> _fileSystemMock;
+        private Mock<IDownloader> _downloaderMock;
+        private Uri Url => new Uri("http://ahdzbook.com/data/out/240/hdwp694087183.jpg");
+        private const string FileName = "hdwp694087183.jpg";
+        private readonly Stream _fileStream = new FileStream(FileName, FileMode.Create, FileAccess.Write, FileShare.ReadWrite);
+
+        [SetUp]
+        public void Setup()
+        {
+            _fileSystemMock = new Mock<IFileSystem>();
+            _downloaderMock = new Mock<IDownloader>();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _fileSystemMock.Verify();
+            _downloaderMock.Verify();
+        }
+
+        [Test]
+        public void VerifyDownloaderManager()
+        {
+            _fileSystemMock.Setup(fileSystem => fileSystem.GenerateFileName(FileName)).Returns(FileName);
+            _fileSystemMock.Setup(fileSystem => fileSystem.PrepareDirectory(FileName)).Verifiable();
+            _fileSystemMock.Setup(fileSystem => fileSystem.CreateStream(FileName)).Returns(_fileStream);
+            _downloaderMock.Setup(downloader => downloader.StartDownload(_fileStream, Url)).Verifiable();
+            
+            var simpleDownloadManager = new SimpleDownloadManager(_fileSystemMock.Object, _downloaderMock.Object, Url);
+            var fileName = simpleDownloadManager.DownloadFile();
+
+            Assert.AreEqual(FileName, fileName, $"Downloaded file should be equal {FileName}");
+        }
+    }
+}
