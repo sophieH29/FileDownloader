6092fe03b95705bb4301752ebf65c28bfd8ae16f
sofia.huts <sofia.huts@justanswer.com>
2017-10-25 18:48:23 +0300

    Refactoring - WIP

---
+++ FileDownloader/Downloaders/BaseDownloader.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileDownloader.Downloaders
+{
+    /// <summary>
+    /// Base implementaion of generic methods for downloading
+    /// </summary>
+    abstract class BaseDownloader
+    {
+        protected int Size;
+        protected int SizeInKb;
+        protected int BytesRead;
+        protected int BytesToRead;
+
+        protected bool _retry;
+        protected int _maxRetry;
+
+       
+
+        /// <summary>
+        /// Retries of execute downloading of specified amount of times
+        /// </summary>
+        /// <param name="method">Method to execute</param>
+        protected void WithRetry(Action method)
+        {
+            int tryCount = 0;
+            bool done = false;
+            do
+            {
+                try
+                {
+                    method();
+                    done = true;
+                }
+                catch (Exception)
+                {
+                    if (tryCount < _maxRetry)
+                    {
+                        tryCount++;
+                        _retry = true;
+                        Console.WriteLine($"Retry #{tryCount} downloading...");
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+            }
+            while (!done);
+        }
+    }
+}
